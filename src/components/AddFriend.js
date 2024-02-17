aa86cc6868c54e9a9cda448a2b8f3570760d5a56
Rycs123 <riyanda.c.sinambela@gmail.com>
2024-02-17 17:05:53 +0700

    feat: add friend with form

---
+++ src/components/AddFriend.js
@@ -0,0 +1,13 @@
+export default function AddFriend() {
+    return (
+        <form action="" className="form-add-friend">
+            <label htmlFor="">🙍🏼‍♂️Nama</label>
+            <input type="text" />
+
+            <label htmlFor="">📸Foto</label>
+            <input type="text" />
+
+            <button className="button">Tambah</button>
+        </form>
+    );
+}
