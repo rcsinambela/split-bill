77f00d7e6f1385a4e63b007cb30c2ad24eb7744d
Rycs123 <riyanda.c.sinambela@gmail.com>
2024-02-18 23:48:39 +0700

    feat(SplitBill): Add form state and inputs for splitting a bill with a friend

--- src/components/SplitBill.js
+++ src/components/SplitBill.js
@@ -1,19 +1,37 @@
+import { useState } from "react";
+
 export default function SplitBill({ selectedFriend }) {
+    const [amount, setAmount] = useState("");
+    const [myBill, setMyBill] = useState("");
+    const friendBill = amount ? amount - myBill : "";
+    const [whoIsPaying, setWhoIsPaying] = useState("user");
+
     return (
         <form action="" className="form-split-bill">
             <h2>Patungan bareng si {selectedFriend.name}</h2>
 
             <label htmlFor="">💵Total Tagihan</label>
-            <input type="text" />
+            <input
+                type="text"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+            />
 
             <label htmlFor="">🙎🏼‍♂️Tagihan Kamu</label>
-            <input type="text" name="" id="" />
+            <input
+                type="text"
+                value={myBill}
+                onChange={(e) => setMyBill(e.target.value)}
+            />
 
             <label htmlFor="">🙋🏼‍♂️Tagihan {selectedFriend.name}</label>
-            <input type="text" name="" id="" disabled />
+            <input type="text" value={friendBill} disabled />
 
             <label htmlFor="">🤑Ditalangin sama</label>
-            <select name="" id="">
+            <select
+                value={whoIsPaying}
+                onChange={(e) => setWhoIsPaying(e.target.value)}
+            >
                 <option value="user">Saya</option>
                 <option value="friend">{selectedFriend.name}</option>
             </select>
