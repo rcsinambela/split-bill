c47540347eab0debf2e2f7be4f6ca75981bf5417
Rycs123 <riyanda.c.sinambela@gmail.com>
2024-02-18 19:12:00 +0700

    feat(AddFriend):  Add friend form with state management

--- src/components/AddFriend.js
+++ src/components/AddFriend.js
@@ -1,11 +1,41 @@
-export default function AddFriend() {
+import { useState } from "react";
+
+export default function AddFriend({ onAddFriend }) {
+    const [name, setName] = useState("");
+    const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        if (!name || !image) return;
+
+        const id = crypto.randomUUID();
+        const newFriend = {
+            id,
+            name,
+            image: `${image}?=${id}`,
+            balance: 0,
+        };
+
+        onAddFriend(newFriend);
+        setName("");
+        setImage("https://i.pravatar.cc/48");
+    }
     return (
-        <form action="" className="form-add-friend">
+        <form action="" className="form-add-friend" onSubmit={handleSubmit}>
             <label htmlFor="">🙍🏼‍♂️Nama</label>
-            <input type="text" />
+            <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
 
             <label htmlFor="">📸Foto</label>
-            <input type="text" />
+            <input
+                type="text"
+                value={image}
+                onChange={(e) => setImage(e.target.value)}
+            />
 
             <button className="button">Tambah</button>
         </form>
