edd3830083c1e3d0ffca6688ae7504ad361f7e81
Rycs123 <riyanda.c.sinambela@gmail.com>
2024-02-18 22:59:50 +0700

    feat(SplitBill): Update FriendList, Friend, and SplitBill components to pass selectedFriend as a prop

--- src/components/SplitBill.js
+++ src/components/SplitBill.js
@@ -1,7 +1,7 @@
-export default function SplitBill() {
+export default function SplitBill({ selectedFriend }) {
     return (
         <form action="" className="form-split-bill">
-            <h2>Patungan bareng si X</h2>
+            <h2>Patungan bareng si {selectedFriend.name}</h2>
 
             <label htmlFor="">💵Total Tagihan</label>
             <input type="text" />
@@ -9,13 +9,13 @@ export default function SplitBill() {
             <label htmlFor="">🙎🏼‍♂️Tagihan Kamu</label>
             <input type="text" name="" id="" />
 
-            <label htmlFor="">🙋🏼‍♂️Tagihan X</label>
+            <label htmlFor="">🙋🏼‍♂️Tagihan {selectedFriend.name}</label>
             <input type="text" name="" id="" disabled />
 
             <label htmlFor="">🤑Ditalangin sama</label>
             <select name="" id="">
                 <option value="user">Saya</option>
-                <option value="friend">X</option>
+                <option value="friend">{selectedFriend.name}</option>
             </select>
 
             <button className="button">Tambah</button>
