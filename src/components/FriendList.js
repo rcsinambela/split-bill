f7b3584af234ca82560247b6e97c7db243797316
Rycs123 <riyanda.c.sinambela@gmail.com>
2024-02-18 22:52:15 +0700

    feat(FriendList): Add onSelectedFriend prop to FriendList component

--- src/components/FriendList.js
+++ src/components/FriendList.js
@@ -1,10 +1,14 @@
 import Friend from "./Friend";
 
-export default function FriendList({ friends }) {
+export default function FriendList({ friends, onSelectedFriend }) {
     return (
         <ul>
             {friends.map((friend, index) => (
-                <Friend friend={friend} key={index}></Friend>
+                <Friend
+                    friend={friend}
+                    key={index}
+                    onSelectedFriend={onSelectedFriend}
+                ></Friend>
             ))}
         </ul>
     );
